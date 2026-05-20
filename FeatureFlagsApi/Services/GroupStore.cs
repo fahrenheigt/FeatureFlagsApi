4852ed5a6393d6bacb8d9eae3571dde33e426290
fahrenheigt <axel.le.meur@outlook.com>
2026-05-20 14:38:12 +0200

    Ajout des groupes

---
+++ FeatureFlagsApi/Services/GroupStore.cs
@@ -0,0 +1,11 @@
+﻿using FeatureFlagsApi.Models;
+
+namespace FeatureFlagsApi.Services;
+
+public static class GroupStore
+{
+    public static List<Group> Groups { get; } = [];
+    private static int _nextId = 1;
+
+    public static int NextId() => _nextId++;
+}
